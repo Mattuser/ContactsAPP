dec73184e0e9c5f4c3464d35ae865620b7cbc506
Matheus Vinicius <matheus.stsousa@gmail.com>
2023-11-28 18:37:42 -0300

    Changing the service name from AccountService to AuthenticationService

--- API/Program.cs
+++ API/Program.cs
@@ -3,32 +3,46 @@ using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
      throw new InvalidOperationException("Connection string 'DefaultConnection' not found!");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    options.UseNpgsql(connectionString);
-});
 
-builder.Services.AddScoped<IAccountService, AccountService>();
+//Add services to DI container
+{
+    var services = builder.Services;
+    services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        options.UseNpgsql(connectionString);
+    });
+    services.AddCors();
+    services.AddControllers();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+    app.UseHttpsRedirection();
+
+    app.MapControllers();
 }
 
-app.UseHttpsRedirection();
-
-app.MapControllers();
 
 app.Run();
